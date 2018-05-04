96ce60bdb6042f27c5bae3dba7fe554704602402
zzf0904 <18811125334@163.com>
2018-05-04 10:22:30 +0800

    第san次提交

--- yuekao/src/js/index.js
+++ yuekao/src/js/index.js
@@ -6,12 +6,28 @@ require.config({
         template: '../js/app/template'
     }
 });
-require(['jquery', 'template'], function() {
+require(['jquery', 'template'], function($, template) {
     $.ajax({
         url: '../mock/data/page1.json',
         success: function(data) {
-            console.log(data);
+            template('#tpl', data, '.page1');
+            fn(); // 详情页
         }
     });
     // 点击切换
+    $('.small-nav>li').each(function(i, v) {
+        $(this).on('click', function() {
+            $('.small-nav>li').eq(i).addClass('on').siblings().removeClass('on');
+            $('.main-nav>ol>li').eq(i).addClass('bg').siblings().removeClass('bg');
+        });
+    });
+    // 详情页
+    function fn() {
+        $('.box').each(function(i, v) {
+            $(this).on('click', function() {
+                var index = $(this).data('src');
+                location.href = index + '.html';
+            });
+        });
+    }
 });
